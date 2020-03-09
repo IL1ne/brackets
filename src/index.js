7473aac5966af7d22d7987dff340efe6c41d048c
IL1ne <mikhailau.kiryl@gmail.com>
2020-03-09 17:01:01 +0300

    feat: add bracket task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  for(let i=0;i<str.length;i++) {
+      for(q=0;q<bracketsConfig.length; q++) {
+        let braketCase="";
+        braketCase=bracketsConfig[q][0]+""+bracketsConfig[q][1];
+
+      if (str.includes(braketCase)) {
+        str = str.replace(braketCase,"");
+        i=0;
+        j=0;
+      }
+    }
+  };
+
+  return str.length == 0;
 }
